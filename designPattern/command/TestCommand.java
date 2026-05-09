d63285cda132234abb15ab5153a184205e95f0a7
nncbtv <ciaodabenito@gmail.com>
2026-05-09 18:58:06 +0200

    Fine Sviluppo Command

--- designPattern/command/TestCommand.java
+++ designPattern/command/TestCommand.java
@@ -6,9 +6,10 @@ public class TestCommand {
 	public static void main(String[] arg) {
 		Operaio matteo = new Operaio("Matteo"); //Questo è il receiver
 		Operaio benito = new Operaio("Benito");
-		Aratura a = new Aratura(benito); //Creo una lavorazione, il nostro comando
+		Aratura a = new Aratura(benito); //Creo una lavorazione
+		//Passo come parametro i receiver per indicare quale classe sa fare il comando
 		Irrigazione b = new Irrigazione(matteo);
-		//Ora li passo all'invoker
+		//Ora gli passo all'invoker
 		BachecaLavorazioni bl = new BachecaLavorazioni(List.of(a,b));
 		//Chiedo all'invoker di inviare una richiesta di esecuzione ai receiver
 		bl.eseguiLavorazioni();
