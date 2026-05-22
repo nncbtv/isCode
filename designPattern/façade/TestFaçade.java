8a7dce78ba19123dfbd6b772ae3bb43811c9f70a
nncbtv <ciaodabenito@gmail.com>
2026-05-22 22:38:53 +0200

    Aggiunta Design Pattern: Façade

--- designPattern/façade/TestFaçade.java
+++ designPattern/façade/TestFaçade.java
@@ -4,9 +4,9 @@ public class TestFaçade {
 
 	public static void main (String[] args) {
 		PiattaformaAziendaleFaçade f = new PiattaformaAziendaleFaçade();
-		System.out.println("Piattaforma per la gestione delle lavorazioni");
+		System.out.println("Piattaforma per la gestione dei compiti aziendali");
 		f.avviaLavorazioneEstiva();
-		System.out.println("\nAvvio un altra lavorazione sulla mia azienda");
+		System.out.println("\nAvvio un altro compito sulla mia azienda");
 		f.avviaIrrigazione();
 		System.out.println("\nChiedo alla piattaforma cosa hanno fatto i miei dipendenti\n");
 		f.lavorazioniSvolte();
