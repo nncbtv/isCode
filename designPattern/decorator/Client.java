483b0f6c935f0d59633a5787dbd90c3e028eedd3
nncbtv <ciaodabenito@gmail.com>
2026-05-28 20:25:35 +0200

    Revisione Pattern: Decoretor, aggiunta dettagli documentazione

--- designPattern/decorator/Client.java
+++ designPattern/decorator/Client.java
@@ -2,16 +2,16 @@ package is.designPattern.decorator;
 
 public class Client {
 	 public static void main(String[] args) {
-	        Bevanda b1 = new Caffe();
+	        Bevanda b1 = new Caffe(); //Componente base
 	        System.out.println(b1.descrizione() + " -> " + b1.costo());
 
-	        Bevanda b2 = new Latte(new Caffe());
+	        Bevanda b2 = new Latte(new Caffe()); //Aggiunta latte al componente base
 	        System.out.println(b2.descrizione() + " -> " + b2.costo());
 	        
-	        Bevanda b3 = new Zucchero(new Caffe());
+	        Bevanda b3 = new Zucchero(new Caffe()); //Aggiunta caffe
 	        System.out.println(b3.descrizione() + " -> " + b3.costo());
 
-	        Bevanda b4 = new Zucchero(new Latte(new Caffe()));
+	        Bevanda b4 = new Zucchero(new Latte(new Caffe())); //Aggiunta latte e zucchero
 	        System.out.println(b4.descrizione() + " -> " + b4.costo());
 	        
 	        
--- designPattern/decorator/DecoratorBevanda.java
+++ designPattern/decorator/DecoratorBevanda.java
@@ -3,7 +3,7 @@ package is.designPattern.decorator;
 public abstract class DecoratorBevanda implements Bevanda{ //Serve ad aggiungere un supplemento oltre alla soluzione base. 
 														   //Utilizza un oggetto bevanda per beneficiare dei suoi metodi
 	protected Bevanda bevanda;							  //in modo da aggiungerne dei supplementi
-
+	//Protected per propagarsi  alle classi figlie
     public DecoratorBevanda(Bevanda bevanda) {
         this.bevanda = bevanda;
     }
